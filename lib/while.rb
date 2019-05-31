ce02b39c6f3a14f993a909da3e359d753b83aee8
Anthony Molina <antonio-molina@att.net>
2019-05-31 03:30:46 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,8 +2,8 @@ def using_while
   levitation_force = 6
 	
   levitation force < 10 
-  while counter < 10 
-  puts "Wingardium Leviosa"
+    while counter < 10 
+    puts "Wingardium Leviosa"
   levitation force += 1
 end
 
